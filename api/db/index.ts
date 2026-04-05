67e7297d381256c60198af6647ab632e2d2f2322
unknown <seifelzeinyyy6@gmail.com>
2026-04-05 23:26:05 +0200

    fix: deploy frontend only first - clean CRA build

--- api/db/index.ts
+++
@@ -1,7 +0,0 @@
-import { neon } from '@neondatabase/serverless';
-import { drizzle } from 'drizzle-orm/neon-http';
-import { config } from '../config';
-import * as schema from './schema';
-
-const sql = neon(config.databaseUrl);
-export const db = drizzle(sql, { schema });
--- api/index.ts
+++
@@ -1,3 +0,0 @@
-import app from './_app';
-
-export default app;
